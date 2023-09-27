a7208da365269509f4d5c24a1e6aec085ac70559
silverfox516 <silverfox516@hotmail.com>
2023-09-28 00:00:02 +0900

    fixed bugs

--- buildscript/setup.sh
+++ buildscript/setup.sh
@@ -5,50 +5,58 @@ source ../shell_color.sh
 
 PRJ=test
 
-# for help
+# for usage
 declare -A TARGETS_INFO
 
-TARGET_FILES=(`ls targets`)
-
+function bs_fatal()
+{
+    echo -e "${COLORS[lightred]}'ctrl + c' to quit, command failed:${COLORS[nc]} $@"
+    while [ 1 ]; do sleep 5; done
+}
 
 function bs_wrapper()
 {
-	if [ "${DEBUG}" == "true" ]; then
-		echo $@
-		return
-	fi
+    if [ "${DEBUG}" == "true" ]; then
+        echo $@
+        return
+    fi
 
-	$@
-	if [ $? -eq 0 ]; then
-		return
-	fi
+    $@
+    if [ $? -eq 0 ]; then
+        return
+    fi
 
-	while [ 1 ]; do
-		echo -e "${COLORS[lightred]}'ctrl + c' to quit, command failed:${COLORS[nc]} $@"
-		sleep 1
-	done
+    bs_fatal $@
 }
 
 function bs_parse_targets() {
-	for t in ${TARGET_FILES[@]}; do
-		source targets/$t
+    DIR_TARGETS=targets
+    TARGET_FILES=(`ls ${DIR_TARGETS}`)
 
-		eval "function ${PRJ}_${t}() {
-			for key in \${!COMMANDS[@]}; do
-				bs_wrapper \${COMMANDS[\$key]};
-			done
-		}"
+    for t in ${TARGET_FILES[@]}; do
+        source ${DIR_TARGETS}/$t
 
-		TARGETS_INFO[${PRJ}_${t}]=${DESC}
-	done
+        # generate target function
+        STR_FUNC="function ${PRJ}_${t}() {"
+        for key in ${!COMMANDS[@]}; do
+            STR_FUNC="${STR_FUNC} bs_wrapper ${COMMANDS[$key]};"
+        done
+        STR_FUNC="${STR_FUNC} }"
+        bs_wrapper eval ${STR_FUNC}
+
+        TARGETS_INFO[${PRJ}_${t}]=${DESC}
+
+        unset COMMANDS
+        unset DESC
+    done
 }
 
-function bs_help() {
-	echo -e "${COLORS[lightcyan]}Available Targets:${COLORS[nc]}"
-	for t in ${!TARGETS_INFO[@]}; do
-		echo -e "  ${COLORS[lightblue]}$t${COLORS[nc]}\t: ${TARGETS_INFO[$t]}"
-	done
+function bs_usage() {
+    echo -e "${COLORS[lightcyan]}Available Targets:${COLORS[nc]}"
+    for t in ${!TARGETS_INFO[@]}; do
+        echo -e "  ${COLORS[lightblue]}$t${COLORS[nc]}\t: ${TARGETS_INFO[$t]}"
+    done
 }
 
 bs_parse_targets
-bs_help
+bs_usage
